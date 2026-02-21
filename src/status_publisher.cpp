aa59afcc37b828372925a7edd212252d7faff793
Harikumaran Ramesh <harikumaran.ramesh@icloud.com>
2026-02-21 22:56:44 +0530

    Implement StatusPublisher class with timer and publisher

--- src/status_publisher.cpp
+++ src/status_publisher.cpp
@@ -4,41 +4,54 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
-// TODO: Include the generated message header
-// #include "ros2_custom_msgs/msg/robot_status.hpp"
+// Include the generated message header
+#include "custom_msgs/msg/robot_status.hpp"
 
 using namespace std::chrono_literals;
-
-/*
- * TODO: Create a Class named 'StatusPublisher' that inherits from rclcpp::Node.
- * Requirements:
- * 1. The constructor should name the node "status_publisher".
- * 2. Create a publisher to topic "/robot_status" with RobotStatus message type.
- * 3. Create a timer that triggers every 1000ms.
- * 4. In timer callback:
- *    - Set robot_name to "Explorer1"
- *    - Decrease battery_level from 100.0 by 0.5 each tick
- *    - Set is_active to true
- *    - Increment mission_count from 0
- *    - Publish and log the message
- */
 
 class StatusPublisher : public rclcpp::Node
 {
 public:
     StatusPublisher()
-        : Node("status_publisher"), battery_level_(100.0), mission_count_(0)
+        : Node("status_publisher"),
+          battery_level_(100.0),
+          mission_count_(0)
     {
-        // TODO: Create publisher here
+        // Create publisher
+        publisher_ = this->create_publisher<custom_msgs::msg::RobotStatus>(
+            "/robot_status", 10);
 
-        // TODO: Create timer here
+        // Create timer (1000 ms)
+        timer_ = this->create_wall_timer(
+            1000ms,
+            std::bind(&StatusPublisher::timer_callback, this));
     }
 
 private:
-    // TODO: Define timer_callback function here
+    void timer_callback()
+    {
+        custom_msgs::msg::RobotStatus msg;
+
+        msg.robot_name = "Explorer1";
+        msg.battery_level = battery_level_;
+        msg.is_active = true;
+        msg.mission_count = mission_count_;
+
+        publisher_->publish(msg);
+
+        RCLCPP_INFO(this->get_logger(),
+                    "Robot: %s | Battery: %.2f | Active: %s | Missions: %d",
+                    msg.robot_name.c_str(),
+                    msg.battery_level,
+                    msg.is_active ? "true" : "false",
+                    msg.mission_count);
+
+        battery_level_ -= 0.5;
+        mission_count_++;
+    }
 
     rclcpp::TimerBase::SharedPtr timer_;
-    // TODO: Declare publisher
+    rclcpp::Publisher<custom_msgs::msg::RobotStatus>::SharedPtr publisher_;
     double battery_level_;
     int32_t mission_count_;
 };
